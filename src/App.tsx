8e28dd963736159000ee58bc3cdcad38432d11e5
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-05-31 22:44:27 -0300

    Estrutura da aplicação
    Configurando a estrutura e testando o funcionamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import React from "react";
+import { GlobalStyle } from "./styles/global";
+
 
 export function App() {
   return (
     <div className="App">
-     
+      <h1> Hello Wolrd </h1>
+      <GlobalStyle />
     </div>
   );
 }
