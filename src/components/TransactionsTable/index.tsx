3b44ebdca754e29ea216c4c07d9ad8582546fb16
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-06-02 17:38:53 -0300

    Componentização
    Componente: TransactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,36 @@
+import { Container } from "./style";
+
+export function TransactionsTable() {
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit">R$ 6000.00</td>
+                        <td>Freelancer</td>
+                        <td>20/02/2021</td>
+                    </tr>
+
+                    <tr >
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$ 400.00</td>
+                        <td>Casa</td>
+                        <td>11/02/2021</td>
+                    </tr>
+
+                  
+                </tbody>
+            </table>
+        </Container>
+    )
+}
