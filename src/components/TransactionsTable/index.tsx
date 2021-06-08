846d40ea60539b55360fafe3ca8398c51a9941d8
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-06-08 18:56:36 -0300

    Modal & Forms
    Formatando valores

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -7,21 +7,21 @@ interface transition {
     title: string;
     amount: number;
     type: string;
-    category: string
+    category: string;
     createAt: string;
 };
 
 export function TransactionsTable() {
 
     const [transactions, setTransactions] = useState<transition[]>([])
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         api.get('/transactions')
-        .then(response => setTransactions(response.data.transactions))
-    },[])
+            .then(response => setTransactions(response.data.transactions))
+    }, [])
 
 
-    return (    
+    return (
         <Container>
             <table>
                 <thead>
@@ -34,17 +34,27 @@ export function TransactionsTable() {
                 </thead>
 
                 <tbody>
-                {transactions.map(transaction => {
-                    return (
+                    {transactions.map(transaction => (
                         <tr key={transaction.id}>
-                        <td>{transaction.title}</td>
-                        <td className={transaction.type}>{transaction.amount}</td>
-                        <td>{transaction.category}</td>
-                        <td>{transaction.createAt}</td>
-                    </tr>
+                            <td>{transaction.title}</td>
+                            <td
+                                className={transaction.type}>
+                                {new Intl.NumberFormat('pt-BR', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(transaction.amount)}
+                            </td>
+                            <td>{transaction.category}</td>
+
+                            <td>
+                            {new Intl.DateTimeFormat('pt-BR').format(
+                                new Date(transaction.createAt)
+                            )}
+                            </td>
+                        </tr>
                     )
-                })}
-                  
+                    )}
+
                 </tbody>
             </table>
         </Container>
