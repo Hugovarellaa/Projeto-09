9e8fa7770927d7793c235e33c611e476dd5d16cb
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-06-20 00:50:17 -0300

    Criando elementos para criar tarefa

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+        <input type="text" />
+        <button type="button"> Adicionar Tarefa </button>
+      </>
+    );
+  }
+}
+
+export default App;
