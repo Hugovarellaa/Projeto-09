5ff7dbb947ff0c56f65b9c403b0a131996fbc356
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-03-24 14:32:08 -0300

    Profile

--- clone-github/src/pages/Profile/index.tsx
+++ clone-github/src/pages/Profile/index.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from "react";
+import { ProfileData } from "../../components/ProfileData";
 
-import { Container } from './styles';
+import { Container, LeftSide, RightSide, Main } from "./styles";
 
 export const Profile: React.FC = () => {
   return (
     <Container>
-      <h1>Profile</h1>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username={"HugoVarella"}
+            name={"Hugo Alves Varella"}
+            avatarUrl={"https://github.com/Hugovarellaa.png"}
+            followers={887}
+            following={3}
+            company={"Casa"}
+            location={"Brasilia-DF, Brasil"}
+            email={"hugovarellaa@gmail.com"}
+            blog={undefined}
+/>
+        </LeftSide>
+        <RightSide></RightSide>
+      </Main>
     </Container>
-  )
-}
-
+  );
+};
